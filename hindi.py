bfabfd42e9dee76a1ef2b3dde0f884f2be3ec21e
Aditya Dhapola <adityadhapola.018@gmail.com>
2019-05-09 19:56:29 +0530

    Update hindi.py

--- hindi.py
+++ hindi.py
@@ -5,7 +5,6 @@ from cltk.tokenize.sentence import TokenizeSentence
 import re
 from cltk.stop.classical_hindi.stops import STOPS_LIST
 
-
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 hindi_text = open('speech.txt','r', encoding = 'utf-8').read()
 hindi_text = re.sub("[a-zA-Z0-9]+", "", hindi_text)
@@ -26,9 +25,7 @@ stop_words = pd.DataFrame(STOPS_LIST)
 final_words = pd.DataFrame()
 final_words = counts[~counts['index'].isin(stop_words[0]).dropna()]
 
-################################################################################################
-# removing all the stop words
-#idf
+# idf
 
 all_unique = counts.author.nunique()
 idf = pd.DataFrame()
@@ -36,7 +33,7 @@ idf = counts.groupby('index').author.nunique()
 idf = idf.reset_index()
 idf['idf'] = np.log(2 / idf['author'])
 
-#tf
+# tf
 
 word_sum = counts.groupby('author').sum()
 tf = pd.DataFrame()
@@ -44,6 +41,6 @@ tf = counts
 tf['word_sum'] = len(hindi_text_tokenize)
 tf['term_frequency'] = tf['word_frequency']/tf['word_sum']
 
-#tf*idf
+# tf*idf
 tf_idf = tf
 tf_idf['tf_idf'] = tf_idf.term_frequency.values * idf['idf']
